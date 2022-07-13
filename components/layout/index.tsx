9b8c47febaa00a459f30362e6fab386d98f2583e
rizkianakbar <rizkianakbar4@gmail.com>
2022-07-13 22:05:06 +0700

    fix: change the title and desc

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -18,8 +18,8 @@ export default function Layout(props: IProps) {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title: 'Rizkan Akbar - Frontend Engineer.',
-    description: `Front-end developer, JavaScript enthusiast`,
+    title: 'CariDev - Search Github Users',
+    description: `CariDev is a search engine for Github users.`,
     image: '',
     type: 'website',
     ...customMeta,
@@ -40,7 +40,7 @@ export default function Layout(props: IProps) {
           href={`https://rizkianakbar.live${router.asPath}`}
         />
         <meta property='og:type' content={meta.type} />
-        <meta property='og:site_name' content='Rizkian Akbar' />
+        <meta property='og:site_name' content='CariDev' />
         <meta property='og:description' content={meta.description} />
         <meta property='og:title' content={meta.title} />
         <meta property='og:image' content={meta.image} />
